b995bcc7bb534fb97e31e0ad45af209773d815ea
neeravjain24 <neeravjain24>
2020-06-29 19:57:55 +0530

    password

---
+++ pattern.py
@@ -0,0 +1,22 @@
+import re
+
+def pw_detection(pw):
+    if len(pw) < 8:
+    	return False
+        
+    elif re.search('[0-9]', pw) is None:
+        return False
+    elif re.search('[A-Z]',pw) is None: 
+        return False
+    elif re.search('[a-z]',pw) is None: 
+        return False
+    else:
+        return True
+
+if __name__ == '__main__':
+    pw = input('Enter your password: ')
+    if password_detection(pw):
+        print('Good, your password is strong!')
+    else:
+        print ('The password is at least eight characters long, contains both uppercase and lowercase characters, and has at least one digit.')
+© 2020 GitHub, Inc.
